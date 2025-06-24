5d11e55928169f49a95476c75d1fb8e8e5035d05
Dimastio Setiawan <dimastio20062@gmail.com>
2025-06-24 22:44:37 +0700

    fix ui, add font

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,32 +1,104 @@
 extends Control
 
-# time_display.gd
-# Attach this script to your "TimeDisplay" Control node.
+# Notes:
+	# 1. Receive from Next js
+	# JavaScriptBridge.get_interface("godotApi").change_player_color = Callable(self, "change_player_color")
 
-# Get a reference to the child TimeLabel node.
-@onready var time_label: Label = $TimeLabel
-
+@onready var user_has_character: bool = false
+@onready var bmi = 18.5
 var clock_update_timer: Timer
 
 func _ready() -> void:
+	clock_time()
+	control_bmi_label()
+	control_motivation_label()
+
+func _on_texture_button_pressed() -> void:
+	$TextureButton.visible = false
+	$InitializeForm.visible = true
+
+func _on_ok_button_pressed() -> void:
+	$InitializeForm.visible = false
+	bmi = snapped($InitializeForm/Weight.value / pow($InitializeForm/Height.value / 100, 2), 0.1)
+	var sanitized_name = $InitializeForm/Name.text.replace("'", "\\'").replace("\"", "\\\"")
+	JavaScriptBridge.eval("window.initializeData({
+		height: '" + str($InitializeForm/Height.value) + "',
+		weight: '" + str($InitializeForm/Weight.value) + "',
+		calories: '" + str($InitializeForm/Calories.value) + "',
+		age: '" + str($InitializeForm/Age.value) + "',
+		bmi: '" + str(bmi) + "',
+		name: '" + sanitized_name + "',
+		 })")
+	user_has_character = true
+	GameManager.update_character($InitializeForm/Height.value, $InitializeForm/Weight.value, $InitializeForm/Calories.value, $InitializeForm/Age.value, bmi, sanitized_name)
+	control_motivation_label()
+	control_bmi_label()
+	control_buttons(false)
+
+func _on_cancel_button_pressed() -> void:
+	if user_has_character:
+		control_buttons(false)
+		$TextureButton.visible = false
+	else:
+		$TextureButton.visible = true
+	$InitializeForm.visible = false
+	$QuestsForm.visible = false
+	$StatsForm.visible = false
+
+func _on_quests_button_pressed() -> void:
+	control_buttons(true)
+	$QuestsForm.visible = true
+
+func _on_stats_button_pressed() -> void:
+	control_buttons(true)
+	$StatsForm.visible = true
+	$StatsForm/Name.text = str(GameManager.character_data['name'])
+	$StatsForm/Height/Value.text = str(GameManager.character_data['height'])
+	$StatsForm/Weight/Value.text = str(GameManager.character_data['weight'])
+	$StatsForm/Age/Value.text = str(GameManager.character_data['age'])
+	$StatsForm/CaloriesBurned/Value.text = str(GameManager.character_data['calories_burned'])
+
+func control_buttons(hide_all: bool) -> void:
+	if user_has_character:
+		$QuestsButton.visible = true
+		$StatsButton.visible = true
+		$TextureButton.visible = false
+	if hide_all:
+		$QuestsButton.visible = false
+		$StatsButton.visible = false
+
+func control_motivation_label() -> void:
+	if user_has_character:
+		$MotivationLabel.position = Vector2(146, 342)
+		$MotivationLabel.text = 'Keep it up!'
+	else:
+		$MotivationLabel.position = Vector2(146, 250)
+		$MotivationLabel.text = '❤️FitQuest'
+		
+func control_bmi_label() -> void:
+	if user_has_character:
+		$BMILabel.visible = true
+		$BMILabel.text = str(bmi)
+	else:
+		$BMILabel.visible = false
+		$BMILabel.text = str(bmi)
+
+# DO NOT TOUCH DO NOT TOUCH DO NOT TOUCH
+# I spent forever making this piece of code to work
+func update_clock_display() -> void:
+	var datetime = get_current_datetime_wib()
+	# Format the time into a "HH:MM" string.
+	var time_string = "%02d:%02d" % [datetime.hour, datetime.minute]
+	$TimeLabel.text = time_string # Update the label's text property.
+
+func clock_time() -> void:
 	# Setup the CLOCK update timer (runs every 1 second)
 	clock_update_timer = Timer.new()
 	add_child(clock_update_timer) # Add timer as a child of this TimeDisplay node
 	clock_update_timer.timeout.connect(update_clock_display)
 	clock_update_timer.wait_time = 1.0
 	clock_update_timer.start()
-	
-	# Run once immediately at the start.
-	update_clock_display()
-
-func update_clock_display() -> void:
-	var datetime = get_current_datetime_wib()
-	
-	# Format the time into a "HH:MM" string.
-	var time_string = "%02d:%02d" % [datetime.hour, datetime.minute]
-	
-	# Update the label's text property.
-	time_label.text = time_string
+	update_clock_display() # Run once immediately at the start.
 
 # Helper function to get the current time in UTC+7
 func get_current_datetime_wib() -> Dictionary:
--- scripts/web3/integration.gd
+++ scripts/web3/integration.gd
@@ -11,124 +11,124 @@ signal error_occurred(error_message)
 var connected_address = ""
 
 func _ready():
-    # IMPORTANT: Check if running in HTML5 export
-    if OS.has_feature("html5"):
-        print("Running in HTML5. Web3 available.")
+	# IMPORTANT: Check if running in HTML5 export
+	if OS.has_feature("html5"):
+		print("Running in HTML5. Web3 available.")
 
-        # Create Callable objects for each GDScript function
-        var on_accounts_changed_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_accounts_changed"))
-        var on_chain_changed_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_chain_changed"))
-        var on_connect_success_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_connect_success"))
-        var on_connect_error_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_connect_error"))
-        var on_sign_message_success_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_sign_message_success"))
-        var on_sign_message_error_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_sign_message_error"))
+		# Create Callable objects for each GDScript function
+		var on_accounts_changed_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_accounts_changed"))
+		var on_chain_changed_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_chain_changed"))
+		var on_connect_success_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_connect_success"))
+		var on_connect_error_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_connect_error"))
+		var on_sign_message_success_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_sign_message_success"))
+		var on_sign_message_error_callable = JavaScriptBridge.create_callback(Callable(self, "_js_on_sign_message_error"))
 
-        # Now, construct the JavaScript string to expose these callables
-        # We'll use JavaScriptBridge.eval() to run this string.
-        # The trick is that the JavaScriptObject returned by create_callback
-        # is automatically converted to a callable JavaScript function when embedded.
-        var js_setup_code = """
-            // Ensure a global object exists to hold our Godot callbacks
-            window.godotCallbacks = window.godotCallbacks || {};
+		# Now, construct the JavaScript string to expose these callables
+		# We'll use JavaScriptBridge.eval() to run this string.
+		# The trick is that the JavaScriptObject returned by create_callback
+		# is automatically converted to a callable JavaScript function when embedded.
+		var js_setup_code = """
+			// Ensure a global object exists to hold our Godot callbacks
+			window.godotCallbacks = window.godotCallbacks || {};
 
-            // Assign the Godot Callables (which become JS functions) to global properties
-            // The references (like '""" + str(on_accounts_changed_callable) + """')
-            // are directly embedded into the string by GDScript.
-            window.godotCallbacks.onAccountsChanged = """ + str(on_accounts_changed_callable) + """;
-            window.godotCallbacks.onChainChanged = """ + str(on_chain_changed_callable) + """;
-            window.godotCallbacks.onConnectSuccess = """ + str(on_connect_success_callable) + """;
-            window.godotCallbacks.onConnectError = """ + str(on_connect_error_callable) + """;
-            window.godotCallbacks.onSignMessageSuccess = """ + str(on_sign_message_success_callable) + """;
-            window.godotCallbacks.onSignMessageError = """ + str(on_sign_message_error_callable) + """;
-        """
-        # Execute the setup code. The second argument is 'use_global_execution_context'.
-        # We want to put these on 'window', so true is appropriate here.
-        JavaScriptBridge.eval(js_setup_code, true) # Set to true to execute in global context
+			// Assign the Godot Callables (which become JS functions) to global properties
+			// The references (like '""" + str(on_accounts_changed_callable) + """')
+			// are directly embedded into the string by GDScript.
+			window.godotCallbacks.onAccountsChanged = """ + str(on_accounts_changed_callable) + """;
+			window.godotCallbacks.onChainChanged = """ + str(on_chain_changed_callable) + """;
+			window.godotCallbacks.onConnectSuccess = """ + str(on_connect_success_callable) + """;
+			window.godotCallbacks.onConnectError = """ + str(on_connect_error_callable) + """;
+			window.godotCallbacks.onSignMessageSuccess = """ + str(on_sign_message_success_callable) + """;
+			window.godotCallbacks.onSignMessageError = """ + str(on_sign_message_error_callable) + """;
+		"""
+		# Execute the setup code. The second argument is 'use_global_execution_context'.
+		# We want to put these on 'window', so true is appropriate here.
+		JavaScriptBridge.eval(js_setup_code, true) # Set to true to execute in global context
 
-    else:
-        print("Not running in HTML5. Web3 functionality disabled.")
+	else:
+		print("Not running in HTML5. Web3 functionality disabled.")
 
 # --- GDScript functions to be called by your game ---
 
 func connect_wallet():
-    if not OS.has_feature("html5"):
-        emit_signal("error_occurred", "Web3 not available outside HTML5 export.")
-        return
+	if not OS.has_feature("html5"):
+		emit_signal("error_occurred", "Web3 not available outside HTML5 export.")
+		return
 
-    # Call the JavaScript function to connect MetaMask
-    # No second argument to eval if not passing any variables from GDScript
-    JavaScriptBridge.eval("connectMetaMask();")
+	# Call the JavaScript function to connect MetaMask
+	# No second argument to eval if not passing any variables from GDScript
+	JavaScriptBridge.eval("connectMetaMask();")
 
 func get_connected_account():
-    return connected_address
+	return connected_address
 
 func sign_message(message: String):
-    if not OS.has_feature("html5"):
-        emit_signal("error_occurred", "Web3 not available outside HTML5 export.")
-        return
-    if connected_address.empty():
-        emit_signal("error_occurred", "No wallet connected to sign message.")
-        return
+	if not OS.has_feature("html5"):
+		emit_signal("error_occurred", "Web3 not available outside HTML5 export.")
+		return
+	if connected_address.empty():
+		emit_signal("error_occurred", "No wallet connected to sign message.")
+		return
 
-    # To pass a string into JavaScript eval, you must manually escape it
-    # and embed it into the JavaScript string literal.
-    # Alternatively, you could use JavaScriptBridge.create_object("String", message)
-    # and pass that object if `eval` could take objects as arguments, but it doesn't work this way.
-    # Manual escaping for 'eval' is often done by wrapping in JSON.stringify() in JS.
-    # Simpler: Call a JS function that takes a direct argument.
-    # The safest way is to make sure your JS function expects string args and they are correctly quoted.
-    # For now, let's just make sure it's wrapped in single quotes for eval.
-    # If the message contains quotes or backslashes, you'd need more robust escaping.
-    # For robust escaping, consider using a JSON-like string if complex data.
-    # For simple string:
-    var escaped_message = message.replace("'", "\\'").replace("\\", "\\\\").replace("\n", "\\n").replace("\r", "\\r");
-    JavaScriptBridge.eval("signMessage('" + escaped_message + "');")
+	# To pass a string into JavaScript eval, you must manually escape it
+	# and embed it into the JavaScript string literal.
+	# Alternatively, you could use JavaScriptBridge.create_object("String", message)
+	# and pass that object if `eval` could take objects as arguments, but it doesn't work this way.
+	# Manual escaping for 'eval' is often done by wrapping in JSON.stringify() in JS.
+	# Simpler: Call a JS function that takes a direct argument.
+	# The safest way is to make sure your JS function expects string args and they are correctly quoted.
+	# For now, let's just make sure it's wrapped in single quotes for eval.
+	# If the message contains quotes or backslashes, you'd need more robust escaping.
+	# For robust escaping, consider using a JSON-like string if complex data.
+	# For simple string:
+	var escaped_message = message.replace("'", "\\'").replace("\\", "\\\\").replace("\n", "\\n").replace("\r", "\\r");
+	JavaScriptBridge.eval("signMessage('" + escaped_message + "');")
 
 # --- Callbacks from JavaScript (Prefix with _js_ for clarity) ---
 
 func _js_on_accounts_changed(args: Array): # Arguments from JS are always an array
-    if args and args.size() > 0 and args[0] is Array and args[0].size() > 0:
-        var new_address = args[0][0] # The JS 'accounts' array is the first element
-        if new_address != connected_address:
-            connected_address = new_address
-            emit_signal("wallet_connected", connected_address)
-            print("Wallet account changed to: ", connected_address)
-    else:
-        connected_address = ""
-        emit_signal("wallet_disconnected")
-        print("Wallet disconnected or accounts cleared.")
+	if args and args.size() > 0 and args[0] is Array and args[0].size() > 0:
+		var new_address = args[0][0] # The JS 'accounts' array is the first element
+		if new_address != connected_address:
+			connected_address = new_address
+			emit_signal("wallet_connected", connected_address)
+			print("Wallet account changed to: ", connected_address)
+	else:
+		connected_address = ""
+		emit_signal("wallet_disconnected")
+		print("Wallet disconnected or accounts cleared.")
 
 func _js_on_chain_changed(args: Array):
-    if args and args.size() > 0:
-        var chain_id = args[0] # Chain ID from JS
-        print("Chain changed to: ", chain_id)
+	if args and args.size() > 0:
+		var chain_id = args[0] # Chain ID from JS
+		print("Chain changed to: ", chain_id)
 
 func _js_on_connect_success(args: Array):
-    if args and args.size() > 0 and args[0] is Array and args[0].size() > 0:
-        connected_address = args[0][0]
-        emit_signal("wallet_connected", connected_address)
-        print("MetaMask connected successfully. Address: ", connected_address)
-    else:
-        emit_signal("error_occurred", "MetaMask connected but no address received.")
+	if args and args.size() > 0 and args[0] is Array and args[0].size() > 0:
+		connected_address = args[0][0]
+		emit_signal("wallet_connected", connected_address)
+		print("MetaMask connected successfully. Address: ", connected_address)
+	else:
+		emit_signal("error_occurred", "MetaMask connected but no address received.")
 
 func _js_on_connect_error(args: Array):
-    var error_message = "Unknown connection error."
-    if args and args.size() > 0:
-        error_message = str(args[0])
-    emit_signal("error_occurred", "MetaMask connection error: " + error_message)
-    print("MetaMask connection error: ", error_message)
+	var error_message = "Unknown connection error."
+	if args and args.size() > 0:
+		error_message = str(args[0])
+	emit_signal("error_occurred", "MetaMask connection error: " + error_message)
+	print("MetaMask connection error: ", error_message)
 
 func _js_on_sign_message_success(args: Array):
-    if args and args.size() > 0:
-        var signature = args[0]
-        emit_signal("message_signed", signature)
-        print("Message signed successfully. Signature: ", signature)
-    else:
-        emit_signal("error_occurred", "Message signed but no signature received.")
+	if args and args.size() > 0:
+		var signature = args[0]
+		emit_signal("message_signed", signature)
+		print("Message signed successfully. Signature: ", signature)
+	else:
+		emit_signal("error_occurred", "Message signed but no signature received.")
 
 func _js_on_sign_message_error(args: Array):
-    var error_message = "Unknown signature error."
-    if args and args.size() > 0:
-        error_message = str(args[0])
-    emit_signal("error_occurred", "MetaMask message signature error: " + error_message)
-    print("MetaMask message signature error: ", error_message)
+	var error_message = "Unknown signature error."
+	if args and args.size() > 0:
+		error_message = str(args[0])
+	emit_signal("error_occurred", "MetaMask message signature error: " + error_message)
+	print("MetaMask message signature error: ", error_message)
